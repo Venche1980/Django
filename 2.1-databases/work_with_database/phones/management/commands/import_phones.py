feeff24484c3622e974d75f5e9984f469072b054
Mihail Tarasov <tarmi@rambler.ru>
2025-03-15 21:33:33 +0300

    Выгрузка каталога товаров из csv-файла с сохранением всех позиций в базе данных

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,7 +1,7 @@
-import csv
-
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+import csv
+from django.utils.text import slugify
 
 
 class Command(BaseCommand):
@@ -13,5 +13,14 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            Phone.objects.create(
+                id=int(phone['id']),
+                name=phone['name'],
+                image=phone['image'],
+                price=int(phone['price']),
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'].lower() == 'true',
+                slug=slugify(phone['name'])
+            )
+            self.stdout.write(self.style.SUCCESS(f'Phone {phone["name"]} imported successfully'))
+
