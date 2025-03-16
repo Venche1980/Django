c0dc4d1c97335b4925a020443af700e6e1529c00
Mihail Tarasov <tarmi@rambler.ru>
2025-03-16 16:30:11 +0300

    Миграции

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,10 +6,13 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
+
+    # Используем prefetch_related для оптимизации запросов
+    students = Student.objects.prefetch_related('teachers').order_by(ordering)
+
+    context = {
+        'object_list': students,
+    }
 
     return render(request, template, context)
